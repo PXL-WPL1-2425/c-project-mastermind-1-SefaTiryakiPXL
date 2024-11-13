f1106c7eed734de782fb1b57bf82500d770ffa99
Sefa Tiryaki <12401207@student.pxl.be>
2024-11-13 14:50:30 +0100

    MasterMind-02 Code Er wordt een random code van 4 kleuren gegenereerd. Om het spel makkelijker te testen wordt deze code in de titel van het window getoond. De 4 kleuren worden willekeurig gekozen uit een reeks van 6 beschikbare kleuren: rood, geel, oranje, wit, groen en blauw.

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,12 +22,31 @@ namespace Mastermind
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            TitelAppearsAbove();
+
         }
 
+        private void TitelAppearsAbove()
+        {
+            Random rnd = new Random();
+            string[] TitelAppears = new string[] { "rood", "geel", "groen", "oranje", "wit", "blauw" };
+            string TitelAppears1 = TitelAppears[rnd.Next(0, TitelAppears.Length)];
+            string TitelAppears2 = TitelAppears[rnd.Next(0, TitelAppears.Length)];
+            string TitelAppears3 = TitelAppears[rnd.Next(0, TitelAppears.Length)];
+            string TitelAppears4 = TitelAppears[rnd.Next(0, TitelAppears.Length)];
+
+            this.Title = $"Mastermind ({TitelAppears1},{TitelAppears2},{TitelAppears3},{TitelAppears4})";
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CheckBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
