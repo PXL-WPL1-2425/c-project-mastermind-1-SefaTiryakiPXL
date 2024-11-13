2ab90c060f60774d01a1ba67fd92162a78cd941d
Sefa Tiryaki <12401207@student.pxl.be>
2024-11-13 18:04:18 +0100

    MasterMind-03 ComboBoxen De 4 comboboxen worden gevuld met de 6 beschikbare kleuren.

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -28,6 +28,34 @@ namespace Mastermind
             InitializeComponent();
             TitelAppearsAbove();
 
+            CheckBox1.Items.Add("rood");
+            CheckBox1.Items.Add("geel");
+            CheckBox1.Items.Add("groen");
+            CheckBox1.Items.Add("oranje");
+            CheckBox1.Items.Add("wit");
+            CheckBox1.Items.Add("blauw");
+
+            CheckBox2.Items.Add("rood");
+            CheckBox2.Items.Add("geel");
+            CheckBox2.Items.Add("groen");
+            CheckBox2.Items.Add("oranje");
+            CheckBox2.Items.Add("wit");
+            CheckBox2.Items.Add("blauw");
+
+            CheckBox3.Items.Add("rood");
+            CheckBox3.Items.Add("geel");
+            CheckBox3.Items.Add("groen");
+            CheckBox3.Items.Add("oranje");
+            CheckBox3.Items.Add("wit");
+            CheckBox3.Items.Add("blauw");
+
+            CheckBox4.Items.Add("rood");
+            CheckBox4.Items.Add("geel");
+            CheckBox4.Items.Add("groen");
+            CheckBox4.Items.Add("oranje");
+            CheckBox4.Items.Add("wit");
+            CheckBox4.Items.Add("blauw");
+
         }
 
         private void TitelAppearsAbove()
